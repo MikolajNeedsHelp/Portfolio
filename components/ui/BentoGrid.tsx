9430ef3849f724aab0783796300da19bb34f61c1
Kolya <example@gmail.com>
2025-02-19 16:07:19 +0100

    finish

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -125,9 +125,9 @@ export const BentoGridItem = ({
 
           {/* Tech stack list div */}
           {id === 3 && (
-            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-1">
               {/* tech stack lists */}
-              <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+              <div className="flex flex-col gap-3 md:gap-3 lg:gap-5">
                 {leftLists.map((item, i) => (
                   <span
                     key={i}
